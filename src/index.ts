b065fbc9008e5c81121a60315cfd5086127308ad
Harshana Amuwatte <harshanaamuwatte@gmail.com>
2026-04-22 11:31:03 +0530

    feat(language-service): implement core service methods for XML processing

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 export { getLanguageService } from "./xmlLanguageService.js";
+
+// Core AST types
 export { XMLDocument, XMLNode, XMLAttribute } from "./parser/xmlNode.js";
+
+// Utility types
+export { Position } from "./utils/positionUtils.js";
+export { Range } from "./utils/rangeUtils.js";
+
+// Service types
+export { CompletionList, CompletionItem } from "./services/xmlCompletion.js";
+export { HoverResult } from "./services/xmlHover.js";
+export { DocumentSymbol } from "./services/xmlSymbols.js";
+export { FoldingRange } from "./services/xmlFolding.js";
+export { TextEdit, FormatterOptions } from "./services/xmlFormatter.js";
+export { DefinitionResult } from "./services/xmlDefinition.js";
+export { ReferenceResult } from "./services/xmlReferences.js";
--- src/xmlLanguageService.ts
+++ src/xmlLanguageService.ts
@@ -1,5 +1,14 @@
 import { parseXMLDocument } from "./parser/xmlParser.js";
 import { XMLDocument } from "./parser/xmlNode.js";
+import { Position } from "./utils/positionUtils.js";
+import { doComplete, CompletionList } from "./services/xmlCompletion.js";
+import { doHover, HoverResult } from "./services/xmlHover.js";
+import { findDocumentSymbols, DocumentSymbol } from "./services/xmlSymbols.js";
+import { getFoldingRanges, FoldingRange } from "./services/xmlFolding.js";
+import { format, TextEdit, FormatterOptions } from "./services/xmlFormatter.js";
+import { doRename } from "./services/xmlRename.js";
+import { doDefinition, DefinitionResult } from "./services/xmlDefinition.js";
+import { findReferences, ReferenceResult } from "./services/xmlReferences.js";
 
 export function getLanguageService() {
   return {
@@ -7,11 +16,36 @@ export function getLanguageService() {
       return parseXMLDocument(uri, text);
     },
 
-    // TODO: doComplete(document: XMLDocument, position: Position): CompletionList
-    // TODO: doHover(document: XMLDocument, position: Position): Hover | null
-    // TODO: findDocumentSymbols(document: XMLDocument): SymbolInformation[]
-    // TODO: getFoldingRanges(document: XMLDocument): FoldingRange[]
-    // TODO: format(document: XMLDocument, options: FormattingOptions): TextEdit[]
-    // TODO: doRename(document: XMLDocument, position: Position, newName: string): WorkspaceEdit | null
+    doComplete(document: XMLDocument, position: Position): CompletionList {
+      return doComplete(document, position);
+    },
+
+    doHover(document: XMLDocument, position: Position): HoverResult | null {
+      return doHover(document, position);
+    },
+
+    findDocumentSymbols(document: XMLDocument): DocumentSymbol[] {
+      return findDocumentSymbols(document);
+    },
+
+    getFoldingRanges(document: XMLDocument): FoldingRange[] {
+      return getFoldingRanges(document);
+    },
+
+    format(document: XMLDocument, options?: FormatterOptions): TextEdit[] {
+      return format(document, options);
+    },
+
+    doRename(document: XMLDocument, position: Position, newName: string): TextEdit[] | null {
+      return doRename(document, position, newName);
+    },
+
+    doDefinition(document: XMLDocument, position: Position): DefinitionResult | null {
+      return doDefinition(document, position);
+    },
+
+    findReferences(document: XMLDocument, position: Position): ReferenceResult[] {
+      return findReferences(document, position);
+    },
   };
 }
